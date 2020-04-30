fb281d9284b97e4f736b7da0e553aec4df63b87a
Akash Chandra Gurava <akash.chandra@live.com>
2020-04-30 15:31:52 +0530

    create transaction page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './ui/pages/homepage.dart';
+import './ui/pages/home_page.dart';
 
 void main() {
   runApp(BajatApp());
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
+import 'transaction_page.dart';
+
 /// Homepage for the app.
 class HomePage extends StatelessWidget {
   // ignore: public_member_api_docs
@@ -18,15 +20,14 @@ class HomePage extends StatelessWidget {
         onPressed: () {
           showCupertinoModalBottomSheet<Widget>(
             context: context,
-            builder: (context, scrollController) => Container(),
+            builder: (context, scrollController) => const TransactionPage(),
           );
         },
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: Container(
+      bottomNavigationBar: const SizedBox(
         height: 50,
-        width: double.infinity,
       ),
     );
   }
